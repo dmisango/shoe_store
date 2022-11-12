8128de4c9d034ab5f55fe188c3e8192356747cc8
Dorcas Misango <111349906+dmisango@users.noreply.github.com>
2022-11-12 15:10:40 +0300

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,60 @@
+let shopItemsData = [
+    {
+      id: "1",
+      name: "NIKE FREE RN 2019 ID",
+      price: 120,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/product-1.png",
+    },
+    {
+        id: "2",
+        name: "NIKE FREE RN 2019 ID",
+        price: 80,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+        img: "images/product-2.png",
+    },
+    {
+        id: "3",
+        name: "NIKE FREE RN 2019 ID",
+        price: 120,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+        img: "images/product-3.png",
+    },
+    {
+        id: "4",
+        name: "NIKE FREE RN 2019 ID",
+        price: 120,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+        img: "images/product-4.png",
+      },
+      {
+        id: "5",
+        name: "NIKE FREE RN 2019 ID",
+        price: 120,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+        img: "images/product-5.png",
+      },
+      {
+        id: "6",
+        name: "NIKE FREE RN 2019 ID",
+        price: 80,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+        img: "images/product-6.png",
+      },
+      {
+        id: "7",
+        name: "NIKE FREE RN 2019 ID",
+        price: 120,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+        img: "images/product-7.png",
+      },
+      {
+        id: "8",
+        name: "NIKE FREE RN 2019 ID",
+        price: 120,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+        img: "images/product-8.png",
+      },
+
+  ];
+  
